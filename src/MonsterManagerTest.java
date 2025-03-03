79f1fd9fbe34b333dd59c9d2493f87d810ce63c1
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-03-02 21:15:01 -0500

    Created test class

---
+++ src/MonsterManagerTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonsterManagerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loadFromFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addMonster() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateMonster() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeMonster() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findHeaviestMonster() {
+    }
+}
